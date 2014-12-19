b0e0236957e7043c8955c6da3207ffa63cb5826f
Joshua Johnston <jojohnston@kaplan.edu>
2014-12-19 14:20:23 -0500

    move patcher into main module to keep it a single file

--- nosegae.py
+++ nosegae.py
@@ -89,11 +89,12 @@ class NoseGAE(Plugin):
                self.assertNotNone(entity.key.id())
 
         Args
-            :param google.appengine.ext.testbed.Testbed the_test: The unittest.TestCase being run
+            :param test: The unittest.TestCase being run
+            :type test: unittest.TestCase
         """
-        import testbed_patch
-        testbed_patch.patch()
         from google.appengine.ext import testbed
+
+        _add_missing_stubs(testbed)
 
         self.testbed = testbed.Testbed()
         self.testbed.activate()
@@ -122,3 +123,33 @@ class NoseGAE(Plugin):
     def stopTest(self, test):
         self.testbed.deactivate()
         del test.test.testbed
+
+
+def _add_missing_stubs(testbed):
+    """Monkeypatch the testbed for stubs that do not have an init method yet"""
+    if not hasattr(testbed, 'PROSPECTIVE_SEARCH_SERVICE_NAME'):
+        from google.appengine.api.prospective_search.prospective_search_stub import ProspectiveSearchStub
+        testbed.PROSPECTIVE_SEARCH_SERVICE_NAME = 'matcher'
+        testbed.INIT_STUB_METHOD_NAMES.update({
+            testbed.PROSPECTIVE_SEARCH_SERVICE_NAME: 'init_prospective_search_stub'
+        })
+
+        def init_prospective_search_stub(self, enable=True, data_file=None):
+            """Workaround to avoid prospective search complain until there is a proper testbed stub
+
+            http://stackoverflow.com/questions/16026703/testbed-stub-for-google-app-engine-prospective-search
+
+            Args:
+                :param self: The Testbed instance.
+                :param enable: True if the fake service should be enabled, False if real
+                    service should be disabled.
+            """
+
+            if not enable:
+                self._disable_stub(testbed.PROSPECTIVE_SEARCH_SERVICE_NAME)
+                return
+            stub = ProspectiveSearchStub(
+                prospective_search_path=data_file,
+                taskqueue_stub=self.get_stub(testbed.TASKQUEUE_SERVICE_NAME))
+            self._register_stub(testbed.PROSPECTIVE_SEARCH_SERVICE_NAME, stub)
+        testbed.Testbed.init_prospective_search_stub = init_prospective_search_stub
